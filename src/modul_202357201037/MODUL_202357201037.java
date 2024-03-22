a39411108ab9b791d0abc2057c07cda0a9394e8f
rinanurrizqi <rinanurrizqi@gmail.com>
2024-03-22 10:19:05 +0700

    ratarata

--- src/modul_202357201037/MODUL_202357201037.java
+++ src/modul_202357201037/MODUL_202357201037.java
@@ -16,12 +16,20 @@ public class MODUL_202357201037 {
     public static void main(String[] args) {
         // TODO code application logic here
         int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 2, 10}};
-        for (int a = 0; a < data.length; a++) {
-            for (int b = 0; b < data[0].length; b++) {
-                System.out.print(data[a][b] + " ");
+        double total = 0;
+        double jumlahelemen = 0;
+        
+        for (int baris = 0; baris < data.length; baris++) {
+            for (int kolom = 0; kolom < data[0].length; kolom++) {
+                System.out.print(data[baris][kolom] + " ");
+                total += data[baris][kolom];
+                jumlahelemen++;
             }
             System.out.print("\n");
         }
+        double ratarata = total/jumlahelemen;
+        System.out.println("Total = " + total);
+        System.out.println("Rata rata = " + ratarata);
     }
     
 }
