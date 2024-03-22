df0414fcba06aecfd3c2a9e714bb469f456f5528
rinanurrizqi <rinanurrizqi@gmail.com>
2024-03-22 10:00:50 +0700

    data[a][b] + " "

--- src/modul_202357201037/MODUL_202357201037.java
+++ src/modul_202357201037/MODUL_202357201037.java
@@ -15,6 +15,13 @@ public class MODUL_202357201037 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int data[][] = {{4, 6, 4, 2, 8, 4, 2, 10}, {4, 6, 4, 2, 8, 4, 2, 10}};
+        for (int a = 0; a < data.length; a++) {
+            for (int b = 0; b < data[0].length; b++) {
+                System.out.print(data[a][b] + " ");
+            }
+            System.out.print("\n");
+        }
     }
     
 }
