86a683430a3b84b681f0d18fb5599cafd9e6dce1
rinanurrizqi <rinanurrizqi@gmail.com>
2024-03-22 11:05:17 +0700

    alamat

--- src/modul_202357201037/alamat.java
+++ src/modul_202357201037/alamat.java
@@ -9,5 +9,14 @@ package modul_202357201037;
  * @author UsEr
  */
 public class alamat {
-    
+    public static void main(String[] args) {
+        String data[][] = {{"ABDUL", "085646668991", "Kediri"}, {"KUSNO", "085646668992", "Trenggalek"}, {"PONIRAN", 
+            "085646668999", "Bojonegoro"}};
+        System.out.println("NAMA\tTELEPON\t\tALAMAT");
+        for (String[] i : data) {
+            System.out.print(i[0] + "\t");
+            System.out.print(i[1] + "\t");
+            System.out.print(i[2] + "\n");
+        }
+    }
 }
